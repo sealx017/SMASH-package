43b305ab9db4561f5bd630c92ede0407fbe67964
sealx017 <48604757+sealx017@users.noreply.github.com>
2023-03-24 11:02:35 -0400

    Update SMASH.py

--- SMASH/SMASH.py
+++ SMASH/SMASH.py
@@ -19,7 +19,7 @@ def SMASH(Y, Cords, len_l = 10, mean_only = "False", kernel_covariance = "All"):
         ----------
         Y: A pandas dataframe of the gene expression profiles, rows representing cells/spots and columns represeneting genes
         
-        Cords: A pandas dataframe of 2D spatial co-ordinates (xy co-ordinates), rownames of Cords should match with that of Y
+        Cords: A pandas dataframe of 2D spatial co-ordinates (xy co-ordinates), rownames of Cords should match the rownames of Y
                        
         len_l: An integer specifying how many lengthscale and period values to consider in the kernel covariance matrices.
                Default is 10
